c342fda15a5eb769046139f612c8d3043c8a8b66
abhishek <36329206+Mario263@users.noreply.github.com>
2023-05-09 16:11:44 +0530

    Project done

---
+++ Desktop/Updated Portfolio/portfolio/src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">loremvbnbefivbfibv</p>
+        <br />
+        <p className="text-xl">fjvnberfiuvnbfivnb</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
