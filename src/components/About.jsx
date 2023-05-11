70ca576bfe93497ac20064e86fd3527f3fac29b3
Abhishek Sharma <36329206+Mario263@users.noreply.github.com>
2023-05-11 15:20:53 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,20 @@ const About = () => {
             About
           </p>
         </div>
-        <p className="text-xl mt-20">loremvbnbefivbfibv</p>
+        <p className="text-xl mt-20">Hello there! I'm Mario, a software enthusiast with a passion for all
+          things tech. With one year of experience building and designing
+          software, I've dabbled in a variety of programming languages, from the
+          classics like C, C++, Java, and Python, to the more cutting-edge
+          fields of Machine Learning and Data Science. But when it comes to
+          building web applications, I'm all about React, Tailwind, and Next.js.</p>
         <br />
-        <p className="text-xl">fjvnberfiuvnbfivnb</p>
+        <p className="text-xl"> I love creating beautiful, functional websites that make people's
+          lives easier and more enjoyable. And if you need some 3D modeling or
+          AR/VR thrown in there, I'm no stranger to Blender and Three.js. When
+          I'm not coding up a storm, you can find me tinkering with MySQL and
+          DBMS, or playing around with OpenCV to bring my projects to life. So
+          whether you need a web app built from scratch or some data analyzed
+          and visualized, I'm your guy. Let's make something amazing together</p>
       </div>
     </div>
   );
